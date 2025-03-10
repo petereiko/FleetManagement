22fa800671914fa2b4c1e93a1a6aa2e9bdc066ef
Hoyte Edorh <edorhsunday@gmail.com>
2025-03-10 19:31:26 +0100

    Account Page Commit

---
+++ FleetManagement.UI/Controllers/Account/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FleetManagement.UI.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
