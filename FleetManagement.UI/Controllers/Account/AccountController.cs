0abe474c9ce8bfba4a1c32fa46680262c46721e8
Hoyte Edorh <edorhsunday@gmail.com>
2025-03-11 16:44:29 +0100

    Login Page

--- FleetManagement.UI/Controllers/Account/AccountController.cs
+++ FleetManagement.UI/Controllers/Account/AccountController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FleetManagement.UI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FleetManagement.UI.Controllers.Account
 {
     public class AccountController : Controller
     {
-        public IActionResult Register()
+        public IActionResult Register(RegisterDto model)
+        {
+            return View();
+        }
+
+        public IActionResult Login(LoginDto model)
         {
             return View();
         }
---
+++ FleetManagement.UI/Models/Dto/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FleetManagement.UI.Models.Dto
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
