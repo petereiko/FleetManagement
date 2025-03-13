a146013eaf940f49a50af6133c7fb249e9060f86
Hoyte Edorh <edorhsunday@gmail.com>
2025-03-13 17:38:23 +0100

    ExpenseList

--- FleetManagement.UI/Controllers/VehicleController.cs
+++ FleetManagement.UI/Controllers/VehicleController.cs
@@ -14,5 +14,18 @@ namespace FleetManagement.UI.Controllers
         {
             return View();
         }
+
+        public IActionResult ExpenseList(FuelExpenseDto model)
+        {
+            // Mock data for demonstration
+            var mockExpenses = new List<FuelExpenseDto>
+            {
+                new FuelExpenseDto { Amount = 5000, State = "Lagos", LGA = "Ikeja", FillingStation = "Total", Date = new DateOnly(2024, 3, 10) },
+                new FuelExpenseDto { Amount = 7000, State = "Abuja", LGA = "Garki", FillingStation = "Mobil", Date = new DateOnly(2024, 3, 9) },
+                new FuelExpenseDto { Amount = 3500, State = "Kano", LGA = "Nassarawa", FillingStation = "NNPC", Date = new DateOnly(2024, 3, 8) }
+            };
+
+            return View(mockExpenses);
+        }
     }
 }
